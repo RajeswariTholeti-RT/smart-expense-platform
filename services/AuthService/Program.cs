775fdf4c46d195160eded5d5ce08d69e2efa21df
Rajeswari Tholeti <276403725+RajeswariTholeti-RT@users.noreply.github.com>
2026-04-19 10:13:59 +0000

    feat(auth): implemented JWT authentication with secure endpoints and Swagger integration

--- services/AuthService/Program.cs
+++ services/AuthService/Program.cs
@@ -1,23 +1,78 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 // Add DbContext for authentication
 builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseInMemoryDatabase("AuthDb"));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer <your_token>'"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var keyString = jwtSettings["Key"] ?? throw new Exception("JWT Key missing");
+var key = Encoding.UTF8.GetBytes(keyString);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Optional - To avoid root URL showing 404 error.
 app.MapGet("/", () => "Welcome to the Auth Service API!");
@@ -34,20 +89,38 @@ app.MapPost("/register", async (AuthDbContext db, User user) =>
     return Results.Ok("User registered successfully");
 });
 
-app.MapPost("/login", async (AuthDbContext db, User login) =>
+app.MapPost("/login", async (AuthDbContext db, IConfiguration config, User login) =>
 {
    var user = db.Users.FirstOrDefault(x => x.Email == login.Email);
 
    if (user == null || !BCrypt.Net.BCrypt.Verify(login.PasswordHash, user.PasswordHash))
         return Results.BadRequest("Invalid email or password");
-    return Results.Ok("Login successful");
+
+     var claims = new[]
+    {
+        new Claim(ClaimTypes.Name, user.Email ?? ""),
+        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+    };
+
+    var keyString = config["Jwt:Key"] ?? throw new Exception("JWT Key missing");
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString));
+    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+    var token = new JwtSecurityToken(
+        issuer: config["Jwt:Issuer"],
+        audience: config["Jwt:Audience"],
+        claims: claims,
+        expires: DateTime.Now.AddHours(1),
+        signingCredentials: creds
+    );
+
+    var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+    return Results.Ok(new { token = jwt });
 });
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapGet("/secure", () => "You are authorized to access this endpoint!")
+    .RequireAuthorization();
 
 app.UseHttpsRedirection();
 
